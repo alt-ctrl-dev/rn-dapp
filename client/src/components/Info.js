eee90ab7701c2f4dcf4b16ab46b0fe1552bfd69c
Coutinho, Reuben K <reuben.coutinho@accenture.com>
2018-04-25 00:57:20 +1000

    Style: Better Responsive UI

--- client/src/components/Info.js
+++ client/src/components/Info.js
@@ -7,12 +7,12 @@ class Info extends Component {
         <div>
 <Row className="margin-botton"> 
    
-   <Col xs={6} sm={6} md={6} lg={6} className="d-flex justify-content-center justify-content-sm-start justify-content-md-start justify-content-lg-start justify-content-xl-start align-items-center">
+   <Col xs={12} sm={12} md={6} lg={6} className="d-flex justify-content-center justify-content-sm-start justify-content-md-start justify-content-lg-start justify-content-xl-start align-items-center">
    <span className="">{this.props.tokens} Token(s) available</span>
    </Col>
 
-   <Col xs={6} sm={6} md={6} lg={6} className="d-flex justify-content-center justify-content-sm-end justify-content-md-end justify-content-lg-endjustify-content-xl-end align-items-center">
-   <span className={(this.props.isOwner)?"owner":""} >Account {this.props.coinbase}</span>
+   <Col xs={12} sm={12} md={6} lg={6} className="d-flex justify-content-center justify-content-sm-start justify-content-md-end justify-content-lg-endjustify-content-xl-end align-items-center">
+   <span className={(this.props.isOwner)?"owner":""} >{`Account ${this.props.coinbase}`}</span>
    </Col>
 
    </Row>
